1d90f5a67346f4cbd6c887b12bf888508c0df8a6
Sergio Obolevich <sergioobolevich@gmail.com>
2021-04-19 19:05:40 -0300

    Add TailwindCSS.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <h1>Hello there from App.js</h1>
+      <h1 className="text-3xl text-blue-500">Hello there from App.js</h1>
     </div>
   );
 }
