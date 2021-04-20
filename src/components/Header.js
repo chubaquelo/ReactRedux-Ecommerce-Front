90e73bedfdea9893f41960e19c10d50f316128e2
Sergio Obolevich <sergioobolevich@gmail.com>
2021-04-19 22:11:01 -0300

    Improve header design. Add responsiveness.

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,55 +2,54 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Header = () => (
-  <header className="bg-gray-200 h-15percent-vh flex flex-row justify-center">
+  <header className="h-15percent-vh flex flex-row justify-center mt-3">
     <div id="logo" className="w-2/12 flex items-center justify-center">
       <Link to="/">
         <img
           src="../assets/logo-jewelry.png"
           alt="Logo"
           className="cursor-pointer"
-          width="110"
+          width="100"
         />
       </Link>
     </div>
-    <div className="header-right-col w-10/12 flex flex-col bg-green-200">
-      <div className="filita-de-arriba bg-green-600 text-right py-1 text-white text-sm pr-3">
-        <p>Exclusive jewelry. Special for gifts.</p>
+    <div className="header-right-col w-10/12 flex flex-col">
+      <div className="filita-de-arriba text-right py-1 text-black text-sm pr-3">
+        <p className="italic text-gray-500 mr-2">Exclusive jewelry. Special for gifts.</p>
       </div>
-      <div className="filita-de-abajo flex flex-row bg-pink-300 h-full">
+      <div className="filita-de-abajo flex flex-row h-full">
         <nav
           id="navbar"
-          className="w-10/12 bg-purple-400 flex items-center w-full"
+          className="w-12/12 md:w-10/12 flex items-center w-full px-6"
         >
           <ul className="flex flex-row w-full justify-around px-10">
             <Link to="/">
-              <li className="cursor-pointer text-white font-bold">Home</li>
+              <li className="cursor-pointer text-black font-bold">Home</li>
             </Link>
-            <Link to="/not-found">
-              <li className="cursor-pointer text-white font-bold">
-                Categories
+            <Link to="/">
+              <li className="cursor-pointer text-black font-bold hidden sm:inline-flex">
+                Products
               </li>
             </Link>
             <Link to="/not-found">
-              <li className="cursor-pointer text-white font-bold">
+              <li className="cursor-pointer text-black font-bold hidden sm:inline-flex">
                 Accesories
               </li>
             </Link>
             <Link to="/not-found">
-              <li className="cursor-pointer text-white font-bold">Services</li>
-            </Link>
-            <Link to="/not-found">
-              <li className="cursor-pointer text-white font-bold">Support</li>
-            </Link>
-            <Link to="/not-found">
-              <li className="cursor-pointer text-white font-bold">Contact</li>
+              <li className="cursor-pointer text-black font-bold hidden sm:inline-flex">
+                Contact
+              </li>
             </Link>
           </ul>
         </nav>
-        <div id="cart-btn" className="flex items-center justify-center w-2/12">
+        <div
+          id="cart-btn"
+          className="flex items-center justify-center w-2/12 hidden lg:inline-flex"
+        >
           <button
             type="button"
-            className="py-2 bg-yellow-500 px-2 cursor-pointer hover:bg-yellow-600 text-white shadow-md"
+            className="py-1 bg-yellow-500 px-2 cursor-pointer hover:bg-yellow-600 text-black shadow-md"
           >
             Cart: 0 Items
           </button>
