8a21da4f37a036773af39d9b5737d52d5fb46015
Sergio Obolevich <sergioobolevich@gmail.com>
2021-04-29 16:34:18 -0300

    Fix failing tests. Add jest to npm scripts.

--- src/components/__tests__/Footer.test.js
+++ src/components/__tests__/Footer.test.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Footer from '../Footer';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 describe('renders correctly', () => {
   it('renders correctly', () => {
-    const tree = renderer.create(<Footer />).toJSON();
+    const tree = renderer
+      .create(
+        <Router>
+          <Footer />
+        </Router>
+      )
+      .toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
--- src/components/__tests__/Header.test.js
+++ src/components/__tests__/Header.test.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Header from '../Header';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 describe('renders correctly', () => {
   it('renders correctly', () => {
-    const tree = renderer.create(<Header />).toJSON();
+    const tree = renderer
+      .create(
+        <Router>
+          <Header />
+        </Router>
+      )
+      .toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
